6a22be11a4d3e2747810b47c69b9f618e122b601
Ludwing08 <pansitas.2001@gmail.com>
2022-12-16 20:22:09 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PiePaginaComponent,
+    EncabezadoComponent,
+    PerfilComponent,
+    HabilidadesComponent,
   ],
   imports: [
     BrowserModule,
